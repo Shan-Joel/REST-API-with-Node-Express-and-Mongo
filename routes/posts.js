00f26c40050680954c456b10b15e5f8e4aef80b9
Shan Joel <shan.joeld@gmail.com>
2022-07-13 00:45:05 +0530

    all done :)

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,72 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Posts');
 
-// Routes
+// Just the index route for posts
 router.get('/', (req, res) => {
    res.send('Posts Page');
 });
 
-router.get('/specific', (req, res) => {
-   res.send('Specific Page');
+// Create a new post
+router.post('/create-post', async (req, res) => {
+   const post = new Post({
+      title: req.body.title,
+      description: req.body.description,
+   });
+
+   try {
+      const savedPost = await post.save();
+      res.json(savedPost);
+   } catch (err) {
+      res.json({ message: err });
+   }
+});
+
+// Retrieve all posts
+router.get('/read-all-posts', async (req, res) => {
+   try {
+      const readPosts = await Post.find();
+      res.json(readPosts);
+   } catch (err) {
+      res.json({ message: err });
+   }
+});
+
+// Specific Post by ID
+router.get('/:postID', async (req, res) => {
+   try {
+      const specificPost = await Post.findById(req.params.postID);
+      res.json(specificPost);
+   } catch (err) {
+      res.json({ message: err });
+   }
+});
+
+// Delete a Specific Post
+router.delete('/:postID', async (req, res) => {
+   try {
+      // const deletePost = await Post.remove({_id: req.params.postID }); //we can use this remove() method as well but I mostly prefer the findByIdAndDelete one!
+      const deletePost = await Post.findByIdAndDelete(req.params.postID);
+      res.json(deletePost);
+   } catch (err) {
+      res.json({ message: err });
+   }
+});
+
+// Update a specific post
+router.patch('/:postId', async (req, res) => {
+   try {
+      const updatePost = await Post.findByIdAndUpdate(
+         //In here also we can use updateOne() method to update a post but I used the findByIdAndUpdate() one
+         { _id: req.params.postId },
+         {
+            $set: req.body,
+         }
+      );
+      res.json(updatePost);
+   } catch (err) {
+      res.json({ message: err });
+   }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
+const port = 3000;
+const URI = process.env.DB_Connection;
 
 // Middlewares - specific route ekak hit karaddi run wenna one specific function ekak thamai simply middleware kiyanne
 // app.use('/posts', () => {
 //    console.log('Run when hit posts route');
 // });
+app.use(bodyParser.json());
+app.use(cors());
 
-// Import routes
-const postsRoute = require('./routes/posts');
+// Import Routes
+const postsRoute = require('./routes/posts'); //In here we are using this as a middleware
 app.use('/posts', postsRoute);
 
-// Connect to DB
-mongoose.connect(process.env.DB_Connection, () => {
-   console.log('DB Connected');
+// Routes
+// Index route
+app.use('/', (req, res) => {
+   res.send('Index Page');
 });
 
+// Connect to DB
+mongoose
+   .connect(URI)
+   .then(() => {
+      console.log('DB Connected!');
+   })
+   .catch((err) => {
+      console.log('DB Connection Error', err);
+   });
+
 // Listen port
-app.listen(3000);
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+});
